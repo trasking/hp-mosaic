26473db5b2df7328929e787b4270226ade36ceb2
jbt <jbt_5@yahoo.com>
2015-11-18 23:06:04 -0800

    launch animation

--- HPMosaic/ViewController.m
+++ HPMosaic/ViewController.m
@@ -10,6 +10,12 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *launchImageViewNarrowWidthConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *launchImageViewWideWidthConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *launchImageViewNarrowAspectConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *launchImageViewWideAspectConstraint;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
 @end
 
 @implementation ViewController
@@ -19,6 +25,19 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self.view layoutIfNeeded];
+    [UIView animateWithDuration:1.0 animations:^{
+        self.launchImageViewNarrowWidthConstraint.active = NO;
+        self.launchImageViewNarrowAspectConstraint.active = NO;
+        self.launchImageViewWideWidthConstraint.active = YES;
+        self.launchImageViewWideAspectConstraint.active = YES;
+        self.titleLabel.alpha = 0.0;
+        [self.view layoutIfNeeded];
+    }];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
